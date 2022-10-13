f03cd02c3223152ad5567990b399cb6a4d4440ca
RafaelEraldo <rafael.v@nagro.app>
2022-10-13 19:47:46 -0300

    feat(RP-12): Removendo metodos não usados

--- src/modules/rural-producers/infra/http/controllers/rural-producers.controller.ts
+++ src/modules/rural-producers/infra/http/controllers/rural-producers.controller.ts
@@ -1,14 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { CreateRuralProducerDto } from 'src/modules/rural-producers/dtos/create-rural-producer.dto';
-import { UpdateRuralProducerDto } from 'src/modules/rural-producers/dtos/update-rural-producer.dto';
 import { RuralProducersService } from 'src/modules/rural-producers/services/rural-producers.service';
 
 @Controller('rural-producers')
@@ -18,28 +9,5 @@ export class RuralProducersController {
   @Post()
   create(@Body() createRuralProducerDto: CreateRuralProducerDto) {
     return this.ruralProducersService.create(createRuralProducerDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.ruralProducersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ruralProducersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateRuralProducerDto: UpdateRuralProducerDto,
-  ) {
-    return this.ruralProducersService.update(+id, updateRuralProducerDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ruralProducersService.remove(+id);
   }
 }
--- src/modules/rural-producers/services/rural-producers.service.ts
+++ src/modules/rural-producers/services/rural-producers.service.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { CreateRuralProducerDto } from '../dtos/create-rural-producer.dto';
-import { UpdateRuralProducerDto } from '../dtos/update-rural-producer.dto';
 import { RuralProducer } from '../infra/database/entities/rural-producer.entity';
 
 @Injectable()
@@ -13,21 +12,5 @@ export class RuralProducersService {
 
   create(createRuralProducerDto: CreateRuralProducerDto) {
     return 'This action adds a new ruralProducer';
-  }
-
-  findAll() {
-    return `This action returns all ruralProducers`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} ruralProducer`;
-  }
-
-  update(id: number, updateRuralProducerDto: UpdateRuralProducerDto) {
-    return `This action updates a #${id} ruralProducer`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} ruralProducer`;
   }
 }
