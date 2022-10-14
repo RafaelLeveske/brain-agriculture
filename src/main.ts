1a3b89a67e3b478c936c65cf8d1558d93a550aa5
RafaelEraldo <rafael.v@nagro.app>
2022-10-14 20:27:04 -0300

    feat(Dockerfile): Implementando configuração de deploy de app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './shared/infra/http/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
